6a7deb1c4afe012cc2fa88bb774501a3998b3af9
ysengoku <ysengoku.tourisme@gmail.com>
2024-03-22 21:58:39 +0100

    Remove useless if condition

--- ft_minishell/exec/redirection.c
+++ ft_minishell/exec/redirection.c
@@ -14,67 +14,25 @@
 
 int	check_redirection(t_base *base, int *fd_in, int *fd_out)
 {
-	if (base->lst->file != NULL)
+	while (base->lst->file)
 	{
-		while (base->lst->file)
+		if (base->lst->file->type == INFILE
+			|| base->lst->file->type == HERE_DOC)
 		{
-			if (base->lst->file->type == INFILE || base->lst->file->type == HERE_DOC)
-			{
-				ft_close(*fd_in, 0);
-				*fd_in = open_infile(base);
-			}
-			else
-			{
-				ft_close(*fd_out, 0);
-				*fd_out = open_outfile(base);
-			}
-			if (*fd_in == -1 || *fd_out == -1)
-			{
-				ft_close(*fd_in, *fd_out);
-				return (1);
-			}
-			base->lst->file = base->lst->file->next;
+			ft_close(*fd_in, 0);
+			*fd_in = open_infile(base);
 		}
+		else
+		{
+			ft_close(*fd_out, 0);
+			*fd_out = open_outfile(base);
+		}
+		if (*fd_in == -1 || *fd_out == -1)
+		{
+			ft_close(*fd_in, *fd_out);
+			return (EXIT_FAILURE);
+		}
+		base->lst->file = base->lst->file->next;
 	}
 	return (0);
 }
-
-// int	check_redirection(t_base *base, int *fd_in, int *fd_out)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	if (base->lst->read[0] != NULL)
-// 	{
-// 		while (base->lst->read[i])
-// 		{
-// 			*fd_in = open_infile(base, i++);
-// 			if (*fd_in == -1)
-// 			{
-// 				if (base->lst->next == NULL)
-// 					free_base(base);
-// 				return (EXIT_FAILURE);
-// 			}
-// 			if (base->lst->read[i])
-// 				ft_close(*fd_in, 0);
-// 		}
-// 	}
-// 	i = 0;
-// 	if (base->lst->write[0] != NULL)
-// 	{
-// 		while (base->lst->write[i])
-// 		{
-// 			*fd_out = open_outfile_trunc(base, i++);
-// 			if (*fd_out == -1)
-// 			{
-// 				ft_close(*fd_in, 0);
-// 				if (base->lst->next == NULL)
-// 					free_base(base);
-// 				return (EXIT_FAILURE);
-// 			}
-// 			if (base->lst->write[i])
-// 				ft_close(*fd_out, 0);
-// 		}
-// 	}
-// 	return (0);
-// }
