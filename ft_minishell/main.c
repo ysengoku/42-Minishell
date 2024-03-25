21ae51d06158cddeda5ce054468b1ef7b0dbe933
yusengok <yusengok@student.42.fr>
2024-03-25 09:23:55 +0100

    Fix norm errors

--- ft_minishell/main.c
+++ ft_minishell/main.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 23:34:48 by dvo               #+#    #+#             */
-/*   Updated: 2024/03/22 16:03:31 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/03/25 09:21:40 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int	main(int ac, char **av, char **env)
 {
-	char 	*str;
+	char	*str;
 	t_base	*base;
 
 	base = ft_calloc(1, sizeof(t_base));
 	base->env = env;
 	(void) ac;
 	(void) av;
-	while(1)
+	while (1)
 	{
 		str = readline(CYAN "minishell >>> " RESET);
 		if (str && *str)
