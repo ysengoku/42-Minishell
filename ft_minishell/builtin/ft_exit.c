cbbb639c9c27458ad63755772a738c41b8e9c47e
yusengok <yusengok@student.42.fr>
2024-03-26 08:52:08 +0100

    Fix norm error & add a free func in main

--- ft_minishell/builtin/ft_exit.c
+++ ft_minishell/builtin/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 11:28:22 by yusengok          #+#    #+#             */
-/*   Updated: 2024/03/25 15:22:37 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/03/26 08:07:06 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int	ft_exit(t_base *base, int exit_status)
 {
 	unlink_heredoc();
-//	rl_clear_history();
+	rl_clear_history();
 	free_base_content(base);
 	free_envlist(base);
 	free(base);
