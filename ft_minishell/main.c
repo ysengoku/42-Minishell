ff9baf1c5b51e92638889a83649ccf8973e5d349
yusengok <yusengok@student.42.fr>
2024-03-26 16:34:44 +0100

    Add exit_code to t_base

--- ft_minishell/main.c
+++ ft_minishell/main.c
@@ -6,16 +6,16 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 23:34:48 by dvo               #+#    #+#             */
-/*   Updated: 2024/03/26 14:45:19 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/03/26 16:32:14 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void handle_sigint(int sig) 
-{ 
+void	handle_sigint(int sig)
+{
 	(void) sig;
-    printf(CYAN "\nminishell >>> "RESET); 
+	printf(CYAN "\nminishell >>> "RESET);
 }
 
 int	main(int ac, char **av, char **env)
@@ -25,28 +25,30 @@ int	main(int ac, char **av, char **env)
 
 	base = ft_calloc(1, sizeof(t_base));
 	if (!base)
-		return(print_error("malloc", "memory allocation failed", EXIT_FAILURE));
+		return (print_error("malloc", "memory allocation failed", EXIT_FAILURE));
 	base->env = env;
+	base->exit_status = 0;
 	(void) ac;
 	(void) av;
 	assign_env(base, env);
 	signal(SIGINT, handle_sigint);
-	while(1)
+	while (1)
 	{
 		str = readline(CYAN "minishell >>> " RESET);
 		if (str && *str)
 		{
 			add_history(str);
 			if (ft_chara_split(str, &base) != -1)
-				ft_exec(base);
+				base->exit_status = ft_exec(base); /// Use for $?
+			printf(RED "exit_status = %d\n" RESET, base->exit_status); ////////////// FOR TEST
 			unlink_heredoc();
 			free_base_content(base);
 		}
-		//else
-		//{
-		//	write(1, "\n", 1);
-			//exit (0);
-		//}
+		if (!str)
+		{
+			write(1, "\n", 1);
+			exit (0);
+		}
 		ft_free((void *)str);
 	}
 	rl_clear_history();
