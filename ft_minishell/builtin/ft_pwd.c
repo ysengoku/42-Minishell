e1b5aeae6ed3cb58ce1c34e1c730e0b75cc3a199
yusengok <ysengoku.tourisme@gmail.com>
2024-03-26 15:15:33 +0100

    Fix norm error

--- ft_minishell/builtin/ft_pwd.c
+++ ft_minishell/builtin/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 08:22:13 by yusengok          #+#    #+#             */
-/*   Updated: 2024/03/25 14:55:17 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/03/26 15:03:16 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,10 @@ int	ft_pwd(t_base *base)
 	char	buf[PWD_SIZE];
 	int		fd_out;
 
-	(void)base;
-	// Need to check all files (in/out) in order
-	// If there is an open error, stop & return (1)
 	if (check_redirection(base, 0, &fd_out) == 1)
 		return (EXIT_FAILURE);
 	if (getcwd(buf, sizeof(buf)) == 0)
-	{
-		//error handling 
-		return (EXIT_FAILURE);
-	}
+		return (ft_perror("getcwd", EXIT_FAILURE));
 	ft_putendl_fd(buf, fd_out);
 	return (0);
 }
