dea052774d814ab705381e033218e6dc0798a8b1
ysengoku <ysengoku.tourisme@gmail.com>
2024-04-01 22:08:23 +0200

    Fix issue with deletd dir (need norminette)

--- ft_minishell/builtin/ft_pwd.c
+++ ft_minishell/builtin/ft_pwd.c
@@ -16,17 +16,34 @@ int	ft_pwd(t_base *base)
 {
 	char	buf[PWD_SIZE];
 	int		fd[2];
+	t_env	*pwd;
 
 	fd[IN] = STDIN_FILENO;
 	fd[OUT] = STDOUT_FILENO;
 	if (check_redirection(base, &fd[IN], &fd[OUT]) == 1)
 		return (1);
-	if (getcwd(buf, sizeof(buf)) == 0)
+	pwd = find_env_var(base, "PWD");
+	if (pwd == NULL)
 	{
-		ft_close(fd[IN], fd[OUT], 1);
-		return (ft_perror("getcwd", 1));
+		if (getcwd(buf, sizeof(buf)) == 0)
+		{
+			ft_close(fd[IN], fd[OUT], 1);
+			return (ft_perror("getcwd", 1));
+		}
+		ft_putendl_fd(buf, fd[OUT]);
 	}
-	ft_putendl_fd(buf, fd[OUT]);
+	else
+		ft_putendl_fd(pwd->value, fd[OUT]);
 	ft_close(fd[IN], fd[OUT], 0);
 	return (0);
 }
+
+/*
+bash-3.2$ pwd
+/Users/yukosengoku/Documents/42/42-Minishell/test
+bash-3.2$ rm -rf ../test
+bash-3.2$ pwd
+/Users/yukosengoku/Documents/42/42-Minishell/test
+bash-3.2$ cd ./
+
+*/
