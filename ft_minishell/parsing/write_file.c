f1f041604611a8c7dcee33963d249d36d1901f4e
yusengok <yusengok@student.42.fr>
2024-04-03 14:47:47 +0200

    suppre old write_file.c

--- ft_minishell/parsing/write_file.c
+++ ft_minishell/parsing/write_file.c
@@ -3,58 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   write_file.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dvo <dvo@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 18:30:31 by dvo               #+#    #+#             */
-/*   Updated: 2024/04/01 22:46:34 by dvo              ###   ########.fr       */
+/*   Updated: 2024/04/03 14:46:45 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-// int	quote_in_file(int *j, char *str, int i, t_file *stock)
-// {
-// 	int	tmp;
-
-// 	tmp = *j;
-// 	if (str[i] == 34)
-// 	{
-// 		i++;
-// 		while (str[i] != 34)
-// 			stock->filename[tmp++] = str[i++] * -1;
-// 	}
-// 	else if (str[i] == 39)
-// 	{
-// 		i++;
-// 		while (str[i] != 39)
-// 			stock->filename[tmp++] = str[i++] * -1;
-// 	}
-// 	i++;
-// 	*j = tmp;
-// 	return (i);
-// }
-
-// int	ft_write_file(char *str, int *i, t_file *stock)
-// {
-// 	int	j;
-
-// 	j = 0;
-// 	if (str[i] == '<' || str[i] == '>')
-// 		return (-1);
-// 	while (str[i] == ' ')
-// 		i++;
-// 	if (str[i] == '<' || str[i] == '>')
-// 		return (-1);
-// 	while (str[i] && str[i] != ' ' && str[i] != '<' \
-// 	&& str[i] != '|' && str[i] != '>')
-// 	{
-// 		if (str[i] == 34 || str[i] == 39)
-// 			i = quote_in_file(&j, str, i, stock);
-// 		stock->filename[j++] = str[i++];
-// 	}
-// 	stock->filename[j] = '\0';
-// 	return (i);
-// }
 
 int	write_out_file(int i, t_line *tmp, char *str, t_base *base)
 {
