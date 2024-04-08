ddb6768c2ff1beca4105a4c41e856bbe302340b3
David.V <david.vo@laposte.net>
2024-04-08 17:43:51 +0200

    early env-i add variable

--- ft_minishell/parsing/chara_split.c
+++ ft_minishell/parsing/chara_split.c
@@ -6,7 +6,7 @@
 /*   By: dvo <dvo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 23:34:58 by dvo               #+#    #+#             */
-/*   Updated: 2024/04/08 15:33:47 by dvo              ###   ########.fr       */
+/*   Updated: 2024/04/08 15:40:07 by dvo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,8 +93,10 @@ int	ft_chara_split(char *s, t_base **base)
 		srep[i] = check_quote(srep[i], *base);
 		if (create_nod(srep[i], *base) == -1)
 		{
-			// free_base_content(*base);
-			return (ft_free_strarr(srep), -1);
+			if ((*base)->lst == NULL)
+				return (ft_free_strarr(srep), -1);
+			else
+				return (0);
 		}
 		i++;
 	}
