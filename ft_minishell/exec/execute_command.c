48e9fa877c0b235f707192ec75f04ae55e1c3aca
yusengok <ysengoku.tourisme@gmail.com>
2024-04-08 14:02:42 +0200

    Fix issue command name with slash & norminette

--- ft_minishell/exec/execute_command.c
+++ ft_minishell/exec/execute_command.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 16:12:40 by yusengok          #+#    #+#             */
-/*   Updated: 2024/04/08 13:47:01 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/04/08 13:59:47 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	execute_command(t_base *base, t_line *node)
 		if (!pathname)
 			exit(error_in_child(base, 1, strerror(errno), NULL));
 	}
-	else if (node->arg[0][0] == '/')
+	else if (ft_strchr(node->arg[0], '/') != NULL)
 		exit(error_in_child(base, 127, node->arg[0], strerror(errno)));
 	else
 		pathname = get_pathname(base, node);
