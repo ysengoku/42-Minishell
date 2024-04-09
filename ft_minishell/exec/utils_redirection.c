f793204aa7a42039b9d0b8f84cb5894ab7cf2235
yusengok <ysengoku.tourisme@gmail.com>
2024-04-09 17:03:45 +0200

    Fix norm error on redirection

--- ft_minishell/exec/utils_redirection.c
+++ ft_minishell/exec/utils_redirection.c
@@ -6,120 +6,84 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 13:13:43 by yusengok          #+#    #+#             */
-/*   Updated: 2024/04/09 14:38:03 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/04/09 16:57:45 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static char	*expand_heredoc_line(t_base *base, char *line);
+static size_t	get_keyend(char *line, size_t start);
 
-int	stock_line_on_heredoc(t_base *base, char *line, int fd_heredoc)
+char	*get_expanded_str(size_t *i, char *line, t_base *base)
 {
-	char	*tmp;
-
-	if (ft_strchr(line, '$') != NULL)
-	{
-		tmp = line;
-		line = expand_heredoc_line(base, line);
-		free(tmp);
-		if (line == NULL)
-			return (1);
-		ft_putendl_fd(line, fd_heredoc);
-	}
-	else
-		ft_putstr_fd(line, fd_heredoc);
-	free(line);
-	return (0);
-}
-
-static char	*expand_heredoc_line(t_base *base, char *line)
-{
-	size_t	i;
 	size_t	j;
-	char	*expanded;
 	char	*buf;
-	char	*tmp;
 	char	key[NAME_MAX];
 	t_env	*target;
 
-	i = 0;
-	expanded = ft_strdup("");
-	if (!expanded)
+	(*i)++;
+	j = get_keyend(line, *i);
+	ft_strlcpy(key, line + *i, j - *i + 1);
+	target = find_env_var(base, key);
+	if (target != NULL)
 	{
-		print_err("minishell", "malloc failed", NULL, 1);
+		buf = ft_strdup(target->value);
+		if (!buf)
+			return (handle_malloc_failure(NULL));
+	}
+	else
+	{
+		buf = ft_calloc(1 + ft_strlen(key) + 1, sizeof(char));
+		if (!buf)
+			return (handle_malloc_failure(NULL));
+		ft_strcpy(buf, "$");
+		ft_strcat(buf, key);
+	}
+	*i = j;
+	return (buf);
+}
+
+static size_t	get_keyend(char *line, size_t start)
+{
+	size_t	end;
+
+	end = start;
+	while (line[end] && line[end] != ' ' && line[end] != '\t'
+		&& line[end] != '$' && line[end] != '\n')
+		end++;
+	return (end);
+}
+
+char	*append_buf(char *expanded_line, char *buf)
+{
+	char	*appended_line;
+
+	appended_line = ft_strjoin(expanded_line, buf);
+	free(expanded_line);
+	free(buf);
+	return (appended_line);
+}
+
+char	*get_str(size_t *i, char *line)
+{
+	char	*buf;
+	size_t	j;
+
+	j = *i;
+	while (line[j] && line[j] != '$' && line[j] != '\n')
+		j++;
+	buf = ft_calloc(j - *i + 2, sizeof(char));
+	if (!buf)
 		return (NULL);
-	}
-	while (line[i])
-	{
-		if (line[i] == '\n')
-			break ;
-		if (line[i] == '$')
-		{
-			i++;
-			j = i;
-			while (line[j] && line[j] != ' ' && line[j] != '\t' && line[j] != '$' && line[j] != '\n')
-				j++;
-			ft_strlcpy(key, line + i, j - i + 1);
-			target = find_env_var(base, key);
-			if (target != NULL)
-			{
-				tmp = expanded;
-				expanded = ft_strjoin(expanded, target->value);
-				free(tmp);
-				if (!expanded)
-				{
-					print_err("minishell", "malloc failed", NULL, 1);
-					return (NULL);
-				}
-			}
-			else
-			{
-				buf = ft_calloc(1 + ft_strlen(key) + 1, sizeof(char));
-				if (!buf)
-				{
-					ft_free((void *)expanded, 1);
-					print_err("minishell", "malloc failed", NULL, 1);
-					return (NULL);
-				}
-				ft_strcpy(buf, "$");
-				ft_strcat(buf, key);
-				tmp = expanded;
-				expanded = ft_strjoin(expanded, buf);
-				free(tmp);
-				free(buf);
-				if (!expanded)
-				{
-					ft_free((void *)key, 1);
-					print_err("minishell", "malloc failed", NULL, 1);
-					return (NULL);
-				}
-			}
-		}
-		else
-		{
-			j = i;
-			while (line[j] && line[j] != '$' && line[j] != '\n')
-				j++;
-			buf = ft_calloc(j - i + 2, sizeof(char));
-			if (!buf)
-			{
-				ft_free((void *)expanded, 1);
-				print_err("minishell", "malloc failed", NULL, 1);
-				return (NULL);
-			}
-			ft_strlcpy(buf, line + i, j - i + 1);
-			tmp = expanded;
-			expanded = ft_strjoin(expanded, buf);
-			free(tmp);
-			free(buf);
-			if (!expanded)
-			{
-				print_err("minishell", "malloc failed", NULL, 1);
-				return (NULL);
-			}
-		}
-		i = j;
-	}
-	return (expanded);
+	ft_strlcpy(buf, line + *i, j - *i + 1);
+	*i = j;
+	return (buf);
+}
+
+char	*handle_malloc_failure(char	*to_free)
+{
+	print_err("minishell", "malloc failed", NULL, 1);
+	if (to_free != NULL)
+		free(to_free);
+	return (NULL);
 }
