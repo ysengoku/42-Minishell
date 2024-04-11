3cd143bf317dba565c515170eff6e14bacbb6b59
yusengok <ysengoku.tourisme@gmail.com>
2024-04-11 15:49:15 +0200

    Fix issue with "cd ~/path_to_dir"

--- ft_minishell/builtin/ft_cd_utils.c
+++ ft_minishell/builtin/ft_cd_utils.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 08:26:03 by yusengok          #+#    #+#             */
-/*   Updated: 2024/04/11 14:11:10 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/04/11 15:42:12 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,66 +49,4 @@ char	*get_path(t_base *base, char *destination)
 		return (NULL);
 	}
 	return (path);
-}
-
-char	*concatenate_path(t_base *base, char *curpath)
-{
-	t_env	*pwd;
-	char	*concatenated_path;
-
-	pwd = find_env_var(base, "PWD");
-	if (pwd == NULL)
-	{
-		ft_free((void *)curpath, 1);
-		return (NULL);
-	}
-	concatenated_path = ft_calloc(ft_strlen(pwd->value)
-			+ ft_strlen(base->lst->arg[1]) + 2, sizeof(char));
-	if (!concatenated_path)
-	{
-		ft_perror("malloc", 1);
-		ft_free((void *)curpath, 1);
-		return (NULL);
-	}
-	ft_strcpy(concatenated_path, pwd->value);
-	if (concatenated_path[ft_strlen(concatenated_path) - 1] != '/')
-		ft_strcat(concatenated_path, "/");
-	ft_strcat(concatenated_path, curpath);
-	free(curpath);
-	return (concatenated_path);
-}
-
-static void	ft_canonicalize(int *src, int *dest, char *curpath)
-{
-	if (curpath[*src] == '/' && curpath[*src + 1] == '.'
-		&& (curpath[*src + 2] == '/' || curpath[*src + 2] == '\0'))
-		*src += 2;
-	else if (curpath[*src] == '/' && curpath[*src + 1] == '.'
-		&& curpath[*src + 2] == '.'
-		&& (curpath[*src + 3] == '/' || curpath[*src + 3] == '\0'))
-	{
-		if (*dest > 0)
-		{
-			while (curpath[--(*dest)] != '/' && *dest > 1)
-				;
-		}
-		*src += 3;
-	}
-	else
-		curpath[(*dest)++] = curpath[(*src)++];
-}
-
-void	canonicalize_path(char *curpath)
-{
-	int	src;
-	int	dest;
-
-	src = 0;
-	dest = 0;
-	while (curpath[src])
-		ft_canonicalize(&src, &dest, curpath);
-	curpath[dest] = '\0';
-	if (!curpath[0])
-		ft_strcpy(curpath, "/");
-	printf("curpath = %s\n", curpath);
 }
---
+++ ft_minishell/builtin/ft_cd_utils2.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cd_utils2.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/11 15:39:05 by yusengok          #+#    #+#             */
+/*   Updated: 2024/04/11 15:41:45 by yusengok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static char	*concatenate_home(t_base *base, char *curpath);
+static char	*concatenate_pwd(t_base *base, char *curpath);
+static void	ft_canonicalize(int *src, int *dest, char *curpath);
+
+char	*concatenate_path(t_base *base, char *curpath)
+{
+	char	*concatenated_path;
+
+	if (ft_strncmp(curpath, "~/", 2) == 0)
+		concatenated_path = concatenate_home(base, curpath);
+	else
+		concatenated_path = concatenate_pwd(base, curpath);
+	free(curpath);
+	return (concatenated_path);
+}
+
+void	canonicalize_path(char *curpath)
+{
+	int	src;
+	int	dest;
+
+	src = 0;
+	dest = 0;
+	while (curpath[src])
+		ft_canonicalize(&src, &dest, curpath);
+	curpath[dest] = '\0';
+	if (!curpath[0])
+		ft_strcpy(curpath, "/");
+}
+
+static char	*concatenate_home(t_base *base, char *curpath)
+{
+	t_env	*home;
+	char	*concatenated_path;
+
+	home = find_env_var(base, "HOME");
+	if (home == NULL)
+		return (NULL);
+	concatenated_path = ft_calloc(ft_strlen(home->value)
+			+ ft_strlen(base->lst->arg[1]) + 2, sizeof(char));
+	if (!concatenated_path)
+	{
+		ft_perror("malloc", 1);
+		return (NULL);
+	}
+	ft_strcpy(concatenated_path, home->value);
+	if (concatenated_path[ft_strlen(concatenated_path) - 1] != '/')
+		ft_strcat(concatenated_path, "/");
+	ft_strcat(concatenated_path, curpath + 2);
+	return (concatenated_path);
+}
+
+static char	*concatenate_pwd(t_base *base, char *curpath)
+{
+	t_env	*pwd;
+	char	*concatenated_path;
+
+	pwd = find_env_var(base, "PWD");
+	if (pwd == NULL)
+		return (NULL);
+	concatenated_path = ft_calloc(ft_strlen(pwd->value)
+			+ ft_strlen(base->lst->arg[1]) + 2, sizeof(char));
+	if (!concatenated_path)
+	{
+		ft_perror("malloc", 1);
+		return (NULL);
+	}
+	ft_strcpy(concatenated_path, pwd->value);
+	if (concatenated_path[ft_strlen(concatenated_path) - 1] != '/')
+		ft_strcat(concatenated_path, "/");
+	ft_strcat(concatenated_path, curpath);
+	return (concatenated_path);
+}
+
+static void	ft_canonicalize(int *src, int *dest, char *curpath)
+{
+	if (curpath[*src] == '/' && curpath[*src + 1] == '.'
+		&& (curpath[*src + 2] == '/' || curpath[*src + 2] == '\0'))
+		*src += 2;
+	else if (curpath[*src] == '/' && curpath[*src + 1] == '.'
+		&& curpath[*src + 2] == '.'
+		&& (curpath[*src + 3] == '/' || curpath[*src + 3] == '\0'))
+	{
+		if (*dest > 0)
+		{
+			while (curpath[--(*dest)] != '/' && *dest > 1)
+				;
+		}
+		*src += 3;
+	}
+	else
+		curpath[(*dest)++] = curpath[(*src)++];
+}
