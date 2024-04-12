7815adfbff5b7eaac9b4f9eaaf0347d7a216045e
yusengok <ysengoku.tourisme@gmail.com>
2024-04-12 16:09:44 +0200

    handle signal debut

--- ft_minishell/main.c
+++ ft_minishell/main.c
@@ -6,11 +6,13 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 23:34:48 by dvo               #+#    #+#             */
-/*   Updated: 2024/04/12 10:54:12 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/04/12 16:07:30 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int received_signal;
 
 static void	handle_sigint(int sig)
 {
@@ -19,42 +21,15 @@ static void	handle_sigint(int sig)
 	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
+	received_signal = SIGINT;
 }
-
-// static void	set_prompt(char *prompt)
-// {
-// 	char	cwd[PATH_MAX];
-// 	char	*user;
-// 	char	*home;
-// 	int		i;
-	
-// 	i = 0;
-// 	getcwd(cwd, PATH_MAX);
-// 	user = getenv("USER");
-// 	home = getenv("HOME");
-// 	ft_strcpy(prompt, user);
-// 	ft_strcat(prompt, "@minishell:");
-// 	if (ft_strncmp(cwd, home, ft_strlen(home)) != 0)
-// 		ft_strcat(prompt, cwd);
-// 	else
-// 	{
-// 		while (home[i] && cwd[i] && cwd[i] == home[i])
-// 			i++;
-// 		ft_strcat(prompt, "~");
-// 		ft_strcat(prompt, cwd + i);
-// 	}
-// 	ft_strcat(prompt, "$ ");
-// }
 
 static void	ft_minishell(t_base *base)
 {
 	char	*str;
 	int		i;
-	// char	prompt[PATH_MAX];
 
 	str = readline(CYAN "minishell >>> " RESET);
-	// set_prompt(prompt);
-	// str = readline(prompt);
 	if (str && *str)
 	{
 		add_history(str);
@@ -62,12 +37,16 @@ static void	ft_minishell(t_base *base)
 			base->exit_code = ft_exec(base);
 		else
 			base->exit_code = 2;
+		received_signal = 0;
 		unlink_heredoc();
 		free_base_content(base);
 	}
 	if (!str)
 	{
-		i = base->exit_code;
+		if (received_signal != 0)
+			i = 128 + received_signal;
+		else
+			i = base->exit_code;
 		free_base_content(base);
 		write(1, "\n", 1);
 		exit (i);
@@ -106,7 +85,14 @@ int	main(int ac, char **av, char **env)
 		assign_env(base);
 		signal(SIGINT, handle_sigint);
 		while (1)
+		{
 			ft_minishell(base);
+			if (received_signal != 0)
+			{
+				base->exit_code = 128 + received_signal;
+				received_signal = 0;
+			}
+		}
 	}
 	else if (ac == 3 && ft_strncmp(av[1], "-c", 2) == 0)
 	{
