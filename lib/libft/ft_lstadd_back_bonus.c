9f2943ecf42ba7aacec51283afd8b18c0bb345c2
yusengok <yusengok@student.42.fr>
2024-04-12 08:52:26 +0200

    Remove non-used comments

--- lib/libft/ft_lstadd_back_bonus.c
+++ lib/libft/ft_lstadd_back_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dvo <marvin@42.fr>                         +#+  +:+       +#+        */
+/*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 17:41:45 by dvo               #+#    #+#             */
-/*   Updated: 2023/11/17 17:41:47 by dvo              ###   ########.fr       */
+/*   Updated: 2024/04/12 08:47:13 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,3 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		tmp->next = new;
 	}
 }
-
-/*int	main(void)
-{
-	t_list * l =  NULL; t_list * l2 =  NULL;
-        ft_lstadd_back(&l, ft_lstnew((void*)1));
-		ft_lstadd_back(&l, ft_lstnew((void*)2));
-		ft_lstadd_back(&l2, ft_lstnew((void*)3));
-        ft_lstadd_back(&l2, ft_lstnew((void*)4));
-        ft_lstadd_back(&l, l2);
-		 free(l);
-}*/
