53a0e3e336d763317b739ff15618cb4b3a5e0419
ysengoku <ysengoku.tourisme@gmail.com>
2024-04-14 21:43:56 +0200

    Add test.md & set 2 func to static on ft_unset

--- ft_minishell/builtin/ft_unset.c
+++ ft_minishell/builtin/ft_unset.c
@@ -13,8 +13,69 @@
 #include "minishell.h"
 
 static int	check_unset_arg(t_base *base, char *str);
+static void	ghost_unset(t_base *base, char *str);
+static void	delete_env(t_base *base, t_line *node, int i);
 static void	delete_node(t_env *node);
-void		ghost_unset(t_base *base, char *str);
+
+int	ft_unset(t_base *base, t_line *node, int fd[2])
+{
+	int		i;
+
+	i = 1;
+	ft_close(fd[IN], fd[OUT], 0);
+	while (node->arg[i])
+	{
+		if (check_unset_arg(base, node->arg[i]) == 1)
+			return (base->exit_code);
+		if (ft_strcmp(node->arg[i], "PWD") == 0 || \
+		ft_strcmp(node->arg[i], "USER") == 0)
+			ghost_unset(base, node->arg[i]);
+		else
+			delete_env(base, node, i);
+		i++;
+	}
+	return (0);
+}
+
+static int	check_unset_arg(t_base *base, char *str)
+{
+	int		i;
+
+	i = 0;
+	if (str[i] == '-')
+	{
+		base->exit_code = 2;
+		return (print_err(UNSET, str, "invalid option", 1));
+	}
+	if (!str[i] || (!ft_isalpha(str[i]) && str[i] != '_'))
+	{
+		base->exit_code = 1;
+		return (print_err(UNSET, str, "not a valid identifier", 1));
+	}
+	while (str[++i])
+	{
+		if (!ft_isalnum(str[i]) && str[i] != '_')
+		{
+			base->exit_code = 1;
+			return (print_err(UNSET, str, "not a valid identifier", 1));
+		}
+	}
+	return (0);
+}
+
+static void	ghost_unset(t_base *base, char *str)
+{
+	t_env	*target_node;
+
+	target_node = base->envn;
+	while (target_node)
+	{
+		if (strcmp(target_node->key, str) == 0)
+			break ;
+		target_node = target_node->next;
+	}
+	target_node->unset = 1;
+}
 
 void	delete_env(t_base *base, t_line *node, int i)
 {
@@ -37,68 +98,6 @@ void	delete_env(t_base *base, t_line *node, int i)
 	else
 		base->envn = target_node->next;
 	delete_node(target_node);
-}
-
-int	ft_unset(t_base *base, t_line *node, int fd[2])
-{
-	int		i;
-
-	i = 1;
-	ft_close(fd[IN], fd[OUT], 0);
-	while (node->arg[i])
-	{
-		if (check_unset_arg(base, node->arg[i]) == 1)
-			return (base->exit_code);
-		if (ft_strcmp(node->arg[i], "PWD") == 0 || \
-		ft_strcmp(node->arg[i], "USER") == 0)
-			ghost_unset(base, node->arg[i]);
-		else
-			delete_env(base, node, i);
-		i++;
-	}
-	return (0);
-}
-
-void	ghost_unset(t_base *base, char *str)
-{
-	t_env	*target_node;
-
-	target_node = base->envn;
-	while (target_node)
-	{
-		if (strcmp(target_node->key, str) == 0)
-			break ;
-		target_node = target_node->next;
-	}
-	target_node->unset = 1;
-}
-
-static int	check_unset_arg(t_base *base, char *str)
-{
-	int		i;
-
-	i = 0;
-	if (str[i] == '-')
-	{
-		base->exit_code = 2;
-		return (print_err(UNSET, str, "invalid option", 1));
-	}
-	if (!str[i]
-		|| (!ft_isalpha(str[i]) && str[i] != '_'))
-	{
-		base->exit_code = 1;
-		return (print_err(UNSET, str, "not a valid identifier", 1));
-	}
-	while (str[++i])
-	{
-		if (!ft_isalnum(str[i]) && str[i] != '_')
-		{
-			base->exit_code = 1;
-			return (print_err(UNSET, str,
-					"not a valid identifier", 1));
-		}
-	}
-	return (0);
 }
 
 static void	delete_node(t_env *node)
