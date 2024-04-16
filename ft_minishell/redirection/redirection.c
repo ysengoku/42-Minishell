ba332a441cc9bb13c15a8ff8fac34a42daa80664
yusengok <ysengoku.tourisme@gmail.com>
2024-04-16 10:12:39 +0200

    Fix issue with a pipe where there are both heredoc & syntax errors redir filename

--- ft_minishell/redirection/redirection.c
+++ ft_minishell/redirection/redirection.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirection.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dvo <dvo@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 07:55:07 by yusengok          #+#    #+#             */
-/*   Updated: 2024/04/14 14:44:40 by dvo              ###   ########.fr       */
+/*   Updated: 2024/04/16 09:37:36 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 static int	check_syntax(t_base *base, t_line *node);
 
-int	check_error_here_doc(t_file	*c_file, int *fd_in, int *fd_out, t_base *base)
-{
-	while (c_file && c_file->filename[0])
-	{
-		if (c_file->type == HERE_DOC)
-		{
-			*fd_in = open_infile(c_file, base, *fd_in);
-			if (*fd_in == -1)
-				return (ft_close(*fd_in, *fd_out, 1));
-		}
-		c_file = c_file->next;
-	}
-	return (ft_close(*fd_in, *fd_out, 1));
-}
+// int	check_error_here_doc(t_file	*c_file, int *fd_in, int *fd_out, t_base *base)
+// {
+// 	while (c_file && c_file->filename[0])
+// 	{
+// 		if (c_file->type == HERE_DOC)
+// 		{
+// 			*fd_in = open_infile(c_file, base, *fd_in);
+// 			if (*fd_in == -1)
+// 				return (ft_close(*fd_in, *fd_out, 1));
+// 		}
+// 		c_file = c_file->next;
+// 	}
+// 	return (ft_close(*fd_in, *fd_out, 1));
+// }
 
 int	check_redirection(t_base *base, t_line *node, int *fd_in, int *fd_out)
 {
@@ -37,7 +37,8 @@ int	check_redirection(t_base *base, t_line *node, int *fd_in, int *fd_out)
 		return (1);
 	current_file = node->file;
 	if (check_syntax(base, node) == 1)
-		return (check_error_here_doc(current_file, fd_in, fd_out, base));
+		return (ft_close(*fd_in, *fd_out, 1));
+		// return (check_error_here_doc(current_file, fd_in, fd_out, base));
 	while (current_file && current_file->filename[0])
 	{
 		if (current_file->type == INFILE || current_file->type == HERE_DOC)
