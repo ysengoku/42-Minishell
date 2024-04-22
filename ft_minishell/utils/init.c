df3635429bcf0503dba8d71936ca590e5e403125
yusengok <yusengok@student.42.fr>
2024-04-22 14:11:36 +0200

    Update shlvl increasement

--- ft_minishell/utils/init.c
+++ ft_minishell/utils/init.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 16:55:17 by yusengok          #+#    #+#             */
-/*   Updated: 2024/04/22 10:37:34 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/04/22 14:09:42 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,26 +28,18 @@ void	check_shell_level(char **env)
 {
 	int		i;
 	int		level;
-	char	*shell;
 	char	*shlevel;
 	char	*tmp;
 
 	i = 0;
 	while (env[i])
 	{
-		if (ft_strncmp(env[i], "SHELL=", 6) == 0)
-			shell = env[i];
-		else if (ft_strncmp(env[i], "SHLVL=", 6) == 0)
+		if (ft_strncmp(env[i], "SHLVL=", 6) == 0)
 			shlevel = env[i];
 		i++;
 	}
-	if (ft_strcmp(shell + 6, "./minishell") == 0)
-	{
-		level = ft_atoi(shlevel + 6) + 1;
-		tmp = ft_itoa(level);
-		ft_strcpy(shlevel + 6, tmp);
-		free(tmp);
-	}
-	else
-		ft_strcpy(shell + 6, "./minishell");
+	level = ft_atoi(shlevel + 6) + 1;
+	tmp = ft_itoa(level);
+	ft_strcpy(shlevel + 6, tmp);
+	free(tmp);
 }
