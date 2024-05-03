747307588f0875344c9f3507a94feb12e5308a04
yusengok <ysengoku.tourisme@gmail.com>
2024-05-03 07:59:31 +0200

    Remove comments from Makefile, move 2 func to error_handling2.c

--- ft_minishell/utils/error_handling2.c
+++ ft_minishell/utils/error_handling2.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 13:50:27 by yusengok          #+#    #+#             */
-/*   Updated: 2024/05/02 17:42:49 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/05/03 07:53:12 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,23 @@ void	exit_after_malloc_fail(t_base *base, void *s, char **arr)
 	if (arr)
 		ft_free_strarr(arr);
 	exit(clear_before_exit(base, 1));
+}
+
+int	free_error_env(t_env *tmp, int i, t_base *base)
+{
+	if (tmp->value)
+		free (tmp->value);
+	free(base->env[i]);
+	base->env[i] = NULL;
+	return (-1);
+}
+
+void	check_error_write_char(t_norm *norm, char *res)
+{
+	if (norm->j == -1)
+	{
+		free(res);
+		res = NULL;
+		norm->i = -1;
+	}
 }
